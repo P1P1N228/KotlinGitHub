8951453e37893a60c6e18c18eda835148369733c
ITStepJunior <ivan.haritoniuk16@gmail.com>
2024-10-31 16:09:41 +0300

    Add new function newData

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,4 +4,8 @@ package org.example
 fun main() {
     val name = "Kotlin"
     println("Hello, " + name + "!")
+    newData()
+}
+fun newData(){
+    println("Версия продукта обновилась до версии 1.0.1")
 }
